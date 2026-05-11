9ab2b9e97cab7dff1c04f0470ee0c46ec632319c
Gabriel Siqueira <gabrielgit987@gmail.com>
2026-05-11 11:46:51 -0300

    Add files via upload

---
+++ Treinos de HTML e CSS/Pagina de uma empresa/custom/js/script.js
@@ -0,0 +1,5 @@
+// CHAMANDO A CLASSE BTN MENU 
+document.querySelector('.btnmenu').addEventListener('click', function(){
+  // SEMPRE QUE CLICAR ELE ADICIONA OU REMOVE
+  document.querySelector('#navegacao').classList.toggle('open')
+})
