174ee5c4bd86855b0a130fca7837ab4d2c921a6c
Gabriel Siqueira <gabrielgit987@gmail.com>
2026-05-13 09:44:08 -0300

    Nossos serviços

--- Treinos de HTML e CSS/Pagina de uma empresa/custom/js/script.js
+++ Treinos de HTML e CSS/Pagina de uma empresa/custom/js/script.js
@@ -1,5 +1,21 @@
 // CHAMANDO A CLASSE BTN MENU 
-document.querySelector('.btnmenu').addEventListener('click', function(){
+document.querySelector('.btnmenu').addEventListener('click', function () {
   // SEMPRE QUE CLICAR ELE ADICIONA OU REMOVE
   document.querySelector('#navegacao').classList.toggle('open')
 })
+
+//MENU FIXO SCROLL
+window.addEventListener('scroll', function(){
+  //VERIFICANDO SE O SCROLL NO EIXO Y E MAIOR OU IGUAL A 10
+  if (window.scrollY >= 10) {
+    // ADICIONA A CLASSE FIXED SCROLL AO ELEMENTO COM ID TOPO
+  document.querySelector('#topo').classList.add('fixed-scroll')
+}
+else{
+  // É O OPOSTO
+   document.querySelector('#topo').classList.remove('fixed-scroll')
+}
+
+
+}
+)
